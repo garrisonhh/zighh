50a48ed0866f9543bd0b8b40bdd83cff8db2acd7
garrisonhh <garrisonhh@pm.me>
2023-08-14 23:08:27 -0400

    build.zig.zon

--- build.zig
+++ build.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule(.{
-        .name = "common",
-        .root_source_file = .{ .path = "src/lib.zig" },
+    _ = b.addModule("common", .{
+        .source_file = .{ .path = "src/main.zig" },
     });
 }
